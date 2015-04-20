4e759a3dacf6c95c9e163780305a9572ac9c90c5
Oliver Maurhart <dyle@dyle.org>
2015-04-20 14:51:02 +0200

    basic structure and files

---
+++ bin/__init__.py
@@ -0,0 +1,35 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
+# ------------------------------------------------------------
+# imap-archiver package file
+# 
+# Copyright (C) 2015  Oliver Maurhart <dyle@dyle.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation version 2.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+# Boston, MA  02110-1301, USA.
+# ------------------------------------------------------------
+
+
+# public modules
+__all__ = []
+
+# package metadata
+__author__      = 'Oliver Maurhart <dyle@dyle.org>'
+__copyright__   = 'Copyright 2015 Oliver Maurhart'
+__license__     = 'GPL v3'
+__licenseurl__  = 'http://www.gnu.org/licenses/gpl.html'
+__title__       = 'imap-archiver'
+__version__     = '0.1'
+
