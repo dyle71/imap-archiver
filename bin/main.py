c1fa2ac28097068658353ae7b152b0bb7100ae39
Oliver Maurhart <oliver.maurhart@ait.ac.at>
2015-04-21 11:44:44 +0200

    basic workflow done

--- bin/main.py
+++ bin/main.py
@@ -26,6 +26,7 @@
 
 import argparse
 import logging
+import getpass
 import sys
 
 import __init__ as imap_archiver
@@ -35,13 +36,37 @@ import __init__ as imap_archiver
 # code
 
 
+def imap_connect(host, port, user, password):
+
+    """Connect to the IMAP server"""
+
+    logging.info('connecting server %s:%d' % (host, port))
+
+
+def imap_disconnect(connect):
+
+    """Disconnect from the IMAP server"""
+
+    logging.info('disconnecting from server')
+
+
+def imap_work(connection):
+
+    """Do the actual work on the IMAP server"""
+    pass
+
+
 def main():
     
     """IMAP-Archiver start"""
 
     # parse arguments
     parser = argparse.ArgumentParser(description = 'IMAP-Archiver')
-    parser.add_argument('-l', '--logging', dest='loglevel', type=int, default=30, help='set logging level (see python logging module) - default is WARNING: 30')
+    parser.add_argument('-t', '--host', dest='host', type=str, help='IMAP host name to connect')
+    parser.add_argument('-p', '--port', dest='port', type=int, default=143, help='IMAP host port to connect')
+    parser.add_argument('-u', '--user', dest='user', type=str, help='user account to log in')
+    parser.add_argument('-k', '--password', dest='password', type=str, help='user password to log in')
+    parser.add_argument('-l', '--logging', dest='loglevel', type=int, default=30, help='set logging level (see python logging module) - default is WARNING: 30 - the lower the more output')
     parser.add_argument('-v', '--version', dest='version', action='store_const', const=True, default=False, help='version information')
     args = parser.parse_args()
 
@@ -52,6 +77,30 @@ def main():
 
     # fix logging
     logging.getLogger(None).setLevel(args.loglevel)
+
+    # check arguments
+    if not args.host:
+        logging.error('no host given. type \'-h\' for help.')
+        sys.exit(1)
+
+    if not (0 <= args.port <= 65535):
+        logging.error('port number invalid. type \'-h\' for help.')
+        sys.exit(1)
+
+    if not args.user:
+        logging.error('no user given. type \'-h\' for help.')
+        sys.exit(1)
+
+    if not args.password:
+        args.password = getpass.getpass('no user password given. plase enter password: ')
+        if not args.password:
+            logging.error('no user password given. type \'-h\' for help.')
+            sys.exit(1)
+
+    # work
+    con = imap_connect(args.host, args.port, args.user, args.password)
+    imap_work(con)
+    imap_disconnect(con)
 
 
 
@@ -67,5 +116,4 @@ def show_version():
 
 if __name__ == "__main__":
     main()
-
 
